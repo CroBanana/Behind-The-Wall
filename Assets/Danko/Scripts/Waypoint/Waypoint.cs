95d80a2a09c1d23f3a2a468aa159c4ddd2944005
Danko <danko.salopek1@gmail.com>
2020-08-10 13:16:08 +0200

    wAYPOIN
    Quest i waypoint

---
+++ Assets/Danko/Scripts/Waypoint/Waypoint.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Waypoint : MonoBehaviour
+{
+    public Image img;
+    public Transform target;
+    public Transform player;
+    public Text meter;
+    public Camera cam;
+    float closeDist;
+
+    private void Start() {
+        player=  GameObject.FindGameObjectWithTag("Player").transform;
+        cam=Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target!=null){
+            GetDistance();
+            CheckOnScreen();
+        }
+    }
+
+    private void GetDistance(){
+        float dist = Vector3.Distance(player.position,target.position);
+        meter.text = dist.ToString("f1")+"m";
+        if(dist<closeDist){
+            
+        }
+    }
+
+    private void CheckOnScreen(){
+        float th= Vector3.Dot((target.position-cam.transform.position).normalized,cam.transform.forward);
+        if(th <=0){
+            ToogleUI(false);
+        }else{
+            ToogleUI(true);
+            transform.position = cam.WorldToScreenPoint(target.position);
+        }
+    }
+    private void ToogleUI(bool value){
+        img.enabled=value;
+        meter.enabled=value;
+    }
+}
