745919cf8ffb5416108118afde01d1a0c55e284d
Dandrlik <33658337+Dandrlik@users.noreply.github.com>
2020-08-19 18:38:05 +0200

    PauseMenuNeRadiJoš

--- Assets/Danko/Scripts/Player2/PlayerInteract.cs
+++ Assets/Danko/Scripts/Player2/PlayerInteract.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerInteract : MonoBehaviour
 {
@@ -37,6 +38,11 @@ public class PlayerInteract : MonoBehaviour
     private RaycastHit hit;
     private Ray ray;
 
+    //DanijelMenu
+    public static bool isPaused;
+    public GameObject pauseMenu, optionsMenu;
+     public GameObject pauseFirstBtn, optionsFirstBtn, optionsClosedBtn;
+
     void Start()
     {
         camera = Camera.main;
@@ -53,6 +59,19 @@ public class PlayerInteract : MonoBehaviour
     {
         CheckIfLookingAtObject();
         ResetIf();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           
+            if (isPaused)
+            {
+                ActivateMenu();
+            }
+            else
+            {
+                DeactivateMenu();
+            }
+        }
+
     }
 
     void CheckIfLookingAtObject()
@@ -106,10 +125,11 @@ public class PlayerInteract : MonoBehaviour
         }
         if (ePressed == false)
         {
-            // menu 
+            
             R();
             I();
         }
+        
     }
 
     void R()
@@ -155,6 +175,46 @@ public class PlayerInteract : MonoBehaviour
                 }
         }
     }
+    //Meni Funkcije
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        canvasInteract.PauseMenu.SetActive(true);
+        canvasInteract.QuestCanvas.SetActive(false);
+        pauseMenu.SetActive(true);
+        
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(pauseFirstBtn);
+
+    }
+
+    void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        isPaused = false;
+    }
+    public void OpenOptions()
+    {
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsFirstBtn);
+    }
+
+    public void CloseOptions()
+    {
+        pauseMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsClosedBtn);
+    }
+
 
     public void Interact()
     {
