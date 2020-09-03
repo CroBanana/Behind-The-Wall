cdde54d4dc1045e024e473f551b020f882e98356
Dandrlik <33658337+Dandrlik@users.noreply.github.com>
2020-09-03 20:53:42 +0200

    OpalaMojaSuStopalaPočelaBitiČujna

---
+++ Assets/RunFX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunFX : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void StepRuning()
+    {
+        AudioClip clip = GetRandomClip();
+        
+        audioSource.PlayOneShot(clip);
+    }
+
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}
---
+++ Assets/StepBackFX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepBackFX : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void StepBack()
+    {
+        AudioClip clip = GetRandomClip();
+        Debug.Log("Run Back");
+        audioSource.PlayOneShot(clip);
+    }
+
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}
